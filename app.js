ce7ea3233ef4146c607ca776dedc8ac1317947e4
shotak <fk.user13@gmail.com>
2022-09-20 13:13:08 +0400

    total rescript nothing old left

--- app.js
+++ app.js
@@ -1,68 +1,165 @@
-"use strict";
-//selecting  elements
-let rock = document.querySelector(".rock");
-let paper = document.querySelector(".paper");
-let scissors = document.querySelector(".scissors");
-let clear = document.querySelector(".clear");
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = "";
 
-//computer choise
-const getComputerChoise = () => {
-  let choiseArry = ["rock", "paper", "scissors"];
-  const randChoise = choiseArry[Math.floor(Math.random() * choiseArry.length)];
-  return randChoise;
+// playround
+const playRound = (playerSelection, computerSelection) => {
+  if (playerSelection === computerSelection) {
+    roundWinner = "Draw";
+  }
+  if (
+    (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+    (playerSelection === "SCISSORS" && computerSelection === "PAPER") ||
+    (playerSelection === "PAPER" && computerSelection === "ROCK")
+  ) {
+    playerScore++;
+    roundWinner = "player";
+  }
+  if (
+    (computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
+    (computerSelection === "SCISSORS" && playerSelection === "PAPER") ||
+    (computerSelection === "PAPER" && playerSelection === "ROCK")
+  ) {
+    computerScore++;
+    roundWinner = "computer";
+  }
+  updateScoreMessage(roundWinner, playerSelection, computerSelection);
 };
-const computerChoise = String(getComputerChoise());
 
-// console.log(typeof computerChoise, computerChoise);
-//listeners
-clear.addEventListener("click", () => {
-  location.reload();
-});
-rock.addEventListener("click", () => {
-  rock = player("rock");
-  console.log(winnerFunc(rock, computerChoise));
-});
-paper.addEventListener("click", () => {
-  paper = player("paper");
-  console.log(winnerFunc(paper, computerChoise));
-});
-scissors.addEventListener("click", () => {
-  scissors = player("scissors");
-  console.log(winnerFunc(scissors, computerChoise));
-});
-
-//player selection
-const player = (value) => {
-  if (value === "rock") {
-    return "rock";
-  } else if (value === "paper") {
-    return "paper";
-  } else if (value === "scissors") {
-    return "scissors";
+const getRandomChoice = () => {
+  let randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return "ROCK";
+  } else if (randomNumber === 1) {
+    return "PAPER";
+  } else if (randomNumber === 2) {
+    return "SCISSORS";
   }
 };
 
-// checking winner
-const winnerFunc = (player, computer) => {
-  if (player === computer) {
-    return "Draw";
-  } else if (player === "rock" && computer === "scissors") {
-    return "player wins rock";
-  } else if (player === "paper" && computer === "rock") {
-    return "player wins paper";
-  } else if (player === "scissors" && computer === "paper") {
-    return "player wins scissors";
-  } else {
-    return `computer wins `;
+const isGameOver = () => {
+  return playerScore === 5 || computerScore === 5;
+};
+
+// ui selecting elements
+
+const scoreInfo = document.getElementById("scoreInfo");
+const scoreMessage = document.getElementById("scoreMessage");
+const playerScorePara = document.getElementById("playerScore");
+const computerScorePara = document.getElementById("computerScore");
+const playerSign = document.getElementById("playerSign");
+const computerSign = document.getElementById("computerSign");
+const rockBtn = document.getElementById("rockBtn");
+const paperBtn = document.getElementById("paperBtn");
+const scissorsBtn = document.getElementById("scissorsBtn");
+const endgameModal = document.getElementById("endgameModal");
+const endgameMsg = document.getElementById("endgameMsg");
+const overlay = document.getElementById("overlay");
+const restartBtn = document.getElementById("restartBtn");
+
+rockBtn.addEventListener("click", () => handleClick("ROCK"));
+paperBtn.addEventListener("click", () => handleClick("PAPER"));
+scissorsBtn.addEventListener("click", () => handleClick("SCISSORS"));
+restartBtn.addEventListener("click", restartGame);
+overlay.addEventListener("click", closeEndgameModal);
+
+const handleClick = (playerSelection) => {
+  if (isGameOver()) {
+    openEndgameModal();
+    return;
   }
 
-  // if (player === "rock" && computer === "scissors") {
-  //   return "player wins with rock";
-  // } else if (player === "paper" && computer === "rock") {
-  //   return "player wins with paper";
-  // } else if (player === "scissors" && computer === "paper") {
-  //   return "player wins with paper";
-  // } else {
-  //   return "computer won ";
-  // }
+  const computerSelection = getRandomChoice();
+  playRound(playerSelection, computerSelection);
+  updateChoices(playerSelection, computerSelection);
+  updateScore();
+
+  if (isGameOver()) {
+    openEndgameModal();
+    setFinalMessage();
+  }
 };
+
+//showing txt content and icons
+function updateChoices(playerSelection, computerSelection) {
+  if (playerSelection === "ROCK") {
+    playerSign.textContent = "✊";
+  } else if (playerSelection === "PAPER") {
+    playerSign.textContent = "✋";
+  } else if (playerSelection === "SCISSORS") {
+    playerSign.textContent = "✌";
+  }
+
+  if (computerSelection === "ROCK") {
+    computerSign.textContent = "✊";
+  } else if (computerSelection === "PAPER") {
+    computerSign.textContent = "✋";
+  } else if (computerSelection === "SCISSORS") {
+    computerSign.textContent = "✌";
+  }
+}
+
+const updateScore = () => {
+  if (roundWinner === "Draw") {
+    scoreInfo.textContent = "It's Draw!";
+  } else if (roundWinner === "player") {
+    scoreInfo.textContent = "You won!";
+  } else if (roundWinner === "computer") {
+    scoreInfo.textContent = "You lost!";
+  }
+
+  playerScorePara.textContent = `Player: ${playerScore}`;
+  computerScorePara.textContent = `Computer: ${computerScore}`;
+};
+
+const updateScoreMessage = (winner, playerSelection, computerSelection) => {
+  if (winner === "player") {
+    scoreMessage.textContent = `${capitalizeFirstLetter(
+      playerSelection
+    )} beats ${computerSelection.toLowerCase()}`;
+    return;
+  }
+  if (winner === "computer") {
+    scoreMessage.textContent = `${capitalizeFirstLetter(
+      playerSelection
+    )} is beaten by ${computerSelection.toLowerCase()}`;
+    return;
+  }
+
+  scoreMessage.textContent = `${capitalizeFirstLetter(
+    playerSelection
+  )} Draw with ${computerSelection.toLowerCase()}`;
+};
+
+const capitalizeFirstLetter = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+};
+
+const openEndgameModal = () => {
+  endgameModal.classList.add("active");
+  overlay.classList.add("active");
+};
+
+function closeEndgameModal() {
+  endgameModal.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+const setFinalMessage = () => {
+  return playerScore > computerScore
+    ? (endgameMsg.textContent = "You won!")
+    : (endgameMsg.textContent = "You lost...");
+};
+
+function restartGame() {
+  playerScore = 0;
+  computerScore = 0;
+  scoreInfo.textContent = "Player Selection";
+  scoreMessage.textContent = "First to score 5 points wins";
+  playerScorePara.textContent = "Player: 0";
+  computerScorePara.textContent = "Computer: 0";
+  playerSign.textContent = "?";
+  computerSign.textContent = "?";
+  endgameModal.classList.remove("active");
+  overlay.classList.remove("active");
+}
